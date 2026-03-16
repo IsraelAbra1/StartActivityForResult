f192cf25ed561853119d33cafbee9330509dd68e
Israel <israel.kugel10@gmail.com>
2026-03-16 21:03:57 +0200

    add the code

--- app/src/main/java/com/example/startactivityforresultprojinclass/MainActivity.java
+++ app/src/main/java/com/example/startactivityforresultprojinclass/MainActivity.java
@@ -2,19 +2,24 @@ package com.example.startactivityforresultprojinclass;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnSetting;
-    ActivityResultLauncher<Intent> resultLauncher;
+    ActivityResultLauncher<Intent> activityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +31,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        init();
+    }
+
+
+    private void init()
+    {
+        btnSetting = findViewById(R.id.btnSetting);
+        btnSetting.setOnClickListener(this);
+
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            Intent data = result.getData();
+
+                            String str = data.getStringExtra("color");
+                            Toast.makeText(MainActivity.this, ""+str, Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnSetting)
+        {
+            Intent i = new Intent(this, SettingActivity.class);
+            activityResultLauncher.launch(i);
+
+        }
+
     }
 }
--- app/src/main/java/com/example/startactivityforresultprojinclass/SettingActivity.java
+++ app/src/main/java/com/example/startactivityforresultprojinclass/SettingActivity.java
@@ -1,24 +1,53 @@
 package com.example.startactivityforresultprojinclass;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner spinner;
+    private String[] arrcolor = { "", "Red", "Blue", "Pink", "Yellow"};
+    private boolean isFirstTime = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_setting);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter aa =
+                new ArrayAdapter(this, android.R.layout.simple_spinner_item,arrcolor);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(aa);
+
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //Toast.makeText(this, "onSelected", Toast.LENGTH_SHORT).show();
+        if(isFirstTime == false)
+        {
+            Intent intent = new Intent();
+            intent.putExtra("color",arrcolor[position]);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+        isFirstTime = false;
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        Toast.makeText(this, "Nothing", Toast.LENGTH_SHORT).show();
+
     }
 }
